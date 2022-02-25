4644049bd2a4b4698a6aeeb5437176031282c7d4
Easton <eastonself@gmail.com>
2022-02-25 13:28:45 -0500

    adding movement back and forth

--- Assets/PhoneTransformController.cs
+++ Assets/PhoneTransformController.cs
@@ -6,8 +6,34 @@ public class PhoneTransformController : MonoBehaviour
 {
 
 
+    [SerializeField]
+    private float _depthMovement = 0;
+
+    [SerializeField]
+    private float _fwdSpeed = 1;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _depthMovement = 1;
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            _depthMovement = -1;
+        }
+        else
+        {
+            _depthMovement = 0;
+        }
+
+        _depthMovement *= (_fwdSpeed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x + _depthMovement, transform.position.y, transform.position.z); 
+    }
+
     private void OnMouseDrag()
     {
         Debug.Log(Input.mousePosition);
+        
     }
 }
