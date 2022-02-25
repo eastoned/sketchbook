cc1a04c29409d68ddabb68ee4f699f6135fd8d61
Easton <eastonself@gmail.com>
2022-02-25 14:41:37 -0500

    eye rig set up

--- Assets/EyeController.cs
+++ Assets/EyeController.cs
@@ -7,10 +7,10 @@ public class EyeController : MonoBehaviour
     [SerializeField]
     private Transform _leftEyeControl, _rightEyeControl;
 
-    private Vector3 _leftEyePosCache, _leftEyeSizeCache;
+    private Vector3 _leftEyePosCache, _leftEyeSizeCache, _rightEyePosCache, _rightEyeSizeCache;
 
     [SerializeField]
-    private float _posXScale, _posYScale;
+    private float _posXScale, _posYScale, _sizeXScale, _sizeYScale;
 
     [SerializeField]
     private Material _eyeMaterial;
@@ -53,15 +53,42 @@ public class EyeController : MonoBehaviour
 
         _leftEyePosCache = _leftEyeControl.localPosition;
         _leftEyeSizeCache = _leftEyeControl.localScale;
+
+        _rightEyePosCache = _rightEyeControl.localPosition;
+        _rightEyeSizeCache = _rightEyeControl.localScale;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float _leftEyePosX = _posXScale*(_leftEyePosCache.x - _leftEyeControl.localPosition.x);
-        float _leftEyePosY = _posYScale * (_leftEyePosCache.y - _leftEyeControl.localPosition.y);
-        
-        _eyeMaterial.SetFloat("_LPosX", 0.25f - _leftEyePosX);
-        _eyeMaterial.SetFloat("_LPosY", 0.5f - _leftEyePosY);
+        float _leftEyePosX = 0.25f - (_posXScale * (_leftEyePosCache.x - _leftEyeControl.localPosition.x));
+        _leftEyePosX = Mathf.Clamp(_leftEyePosX, 0, 0.5f);
+        float _leftEyePosY = 0.5f - (_posYScale * (_leftEyePosCache.y - _leftEyeControl.localPosition.y));
+        _leftEyePosY = Mathf.Clamp(_leftEyePosY, 0.3f, 0.75f);
+
+        float _leftEyeSizeX = .1f - (_sizeXScale*(_leftEyeSizeCache.x - _leftEyeControl.localScale.x));
+        _leftEyeSizeX = Mathf.Clamp(_leftEyeSizeX, .01f, .25f);
+        float _leftEyeSizeY = .15f - ( _sizeYScale*(_leftEyeSizeCache.y - _leftEyeControl.localScale.y));
+        _leftEyeSizeY = Mathf.Clamp(_leftEyeSizeY, 0.02f, .25f);
+
+        float _rightEyePosX = 0.75f - (_posXScale * (_rightEyePosCache.x - _rightEyeControl.localPosition.x));
+        _rightEyePosX = Mathf.Clamp(_rightEyePosX, 0.5f, 1f);
+        float _rightEyePosY = 0.5f - (_posYScale * (_rightEyePosCache.y - _rightEyeControl.localPosition.y));
+        _rightEyePosY = Mathf.Clamp(_rightEyePosY, 0.3f, 0.75f);
+
+        float _rightEyeSizeX = .1f - (_sizeXScale * (_rightEyeSizeCache.x - _rightEyeControl.localScale.x));
+        _rightEyeSizeX = Mathf.Clamp(_rightEyeSizeX, .01f, .25f);
+        float _rightEyeSizeY = .15f - (_sizeYScale * (_rightEyeSizeCache.y - _rightEyeControl.localScale.y));
+        _rightEyeSizeY = Mathf.Clamp(_rightEyeSizeY, 0.02f, .25f);
+
+        _eyeMaterial.SetFloat("_LPosX", _leftEyePosX);
+        _eyeMaterial.SetFloat("_LPosY", _leftEyePosY);
+        _eyeMaterial.SetFloat("_LSizeX", _leftEyeSizeX);
+        _eyeMaterial.SetFloat("_LSizeY", _leftEyeSizeY);
+
+        _eyeMaterial.SetFloat("_RPosX", _rightEyePosX);
+        _eyeMaterial.SetFloat("_RPosY", _rightEyePosY);
+        _eyeMaterial.SetFloat("_RSizeX", _rightEyeSizeX);
+        _eyeMaterial.SetFloat("_RSizeY", _rightEyeSizeY);
     }
 }
