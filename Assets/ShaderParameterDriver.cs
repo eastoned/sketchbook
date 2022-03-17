ad0ee88bf5300d9a431d3625cd1326fea3b213c0
Easton <eastonself@gmail.com>
2022-03-17 17:25:45 -0400

    nice ik testing

--- Assets/ShaderParameterDriver.cs
+++ Assets/ShaderParameterDriver.cs
@@ -15,11 +15,6 @@ public class ShaderParameterDriver : MonoBehaviour
     [Range(0, 1)]
     private float _gameTime;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     // Update is called once per frame
     void Update()
     {
