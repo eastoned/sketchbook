f641940f353fc50cc12da5be837aeb6d23f3b2ae
Easton <eastonself@gmail.com>
2022-03-23 18:18:24 -0400

    some cross product eye stuff

--- Assets/EyeController.cs
+++ Assets/EyeController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class EyeController : MonoBehaviour
 {
     [SerializeField]
-    private Transform _leftEyeControl, _rightEyeControl;
+    private Transform _leftEyeControl, _rightEyeControl, _headTransform, _eyeTarget;
+
+    [SerializeField]
+    private Vector3 _eyeOffset;
 
     private Vector3 _leftEyePosCache, _leftEyeSizeCache, _rightEyePosCache, _rightEyeSizeCache;
 
@@ -17,6 +20,8 @@ public class EyeController : MonoBehaviour
 
     private float _eyeTimer;
 
+
+
     float _leftEyePosX;
     float _leftEyePosY;
     float _leftEyeSizeX;
@@ -26,6 +31,8 @@ public class EyeController : MonoBehaviour
     float _rightEyePosY;
     float _rightEyeSizeX;
     float _rightEyeSizeY;
+
+    public Vector2 _eyeDiff;
 
     //mapping params
     /*
@@ -103,33 +110,81 @@ public class EyeController : MonoBehaviour
             _eyeTimer = Random.Range(.2f, 2f);
         }*/
 
-        _leftEyePosX = Mathf.PerlinNoise(Time.time, 0);
-        _leftEyePosY = Mathf.PerlinNoise(Time.time, 1);
-        _leftEyeSizeX = Mathf.PerlinNoise(Time.time, 2);
-        _leftEyeSizeY = Mathf.PerlinNoise(Time.time, 3);
+        //_leftEyePosX = Mathf.PerlinNoise(Time.time, 0);
+        //_leftEyePosY = Mathf.PerlinNoise(Time.time, 1);
+        //_leftEyeSizeX = Mathf.PerlinNoise(Time.time, 2);
+        //_leftEyeSizeY = Mathf.PerlinNoise(Time.time, 3);
 
         
-        _leftEyePosY = Mathf.Lerp(.3f, .75f, _leftEyePosY);
-        _leftEyeSizeX = Mathf.Lerp(.01f, .25f, _leftEyeSizeX);
-        _leftEyeSizeY = Mathf.Lerp(.02f, .25f, _leftEyeSizeY);
+        //_leftEyePosY = Mathf.Lerp(.3f, .75f, _leftEyePosY);
+        //_leftEyeSizeX = Mathf.Lerp(.01f, .25f, _leftEyeSizeX);
+        //_leftEyeSizeY = Mathf.Lerp(.02f, .25f, _leftEyeSizeY);
 
-        _rightEyePosX = Mathf.Lerp(.5f, 1f, _leftEyePosX);
-        _rightEyePosY = _leftEyePosY;
-        _rightEyeSizeX = _leftEyeSizeX;
-        _rightEyeSizeY = _leftEyeSizeY;
+        //_rightEyePosX = Mathf.Lerp(.5f, 1f, _leftEyePosX);
+        //_rightEyePosY = _leftEyePosY;
+        //_rightEyeSizeX = _leftEyeSizeX;
+        //_rightEyeSizeY = _leftEyeSizeY;
 
-        _eyeTimer -= Time.deltaTime;
+        //_eyeTimer -= Time.deltaTime;
 
-        _leftEyePosX = Mathf.Lerp(0f, .5f, _leftEyePosX);
+        //_leftEyePosX = Mathf.Lerp(0f, .5f, _leftEyePosX);
 
         _eyeMaterial.SetFloat("_LPosX", _leftEyePosX);
         _eyeMaterial.SetFloat("_LPosY", _leftEyePosY);
-        _eyeMaterial.SetFloat("_LSizeX", _leftEyeSizeX);
-        _eyeMaterial.SetFloat("_LSizeY", _leftEyeSizeY);
+       // _eyeMaterial.SetFloat("_LSizeX", _leftEyeSizeX);
+       // _eyeMaterial.SetFloat("_LSizeY", _leftEyeSizeY);
 
         _eyeMaterial.SetFloat("_RPosX", _rightEyePosX);
         _eyeMaterial.SetFloat("_RPosY", _rightEyePosY);
         _eyeMaterial.SetFloat("_RSizeX", _rightEyeSizeX);
         _eyeMaterial.SetFloat("_RSizeY", _rightEyeSizeY);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Vector3 _eyePosVec = _headTransform.localPosition + _eyeOffset;
+        _eyePosVec = _headTransform.TransformPoint(_eyePosVec);
+
+        Gizmos.DrawRay(_eyePosVec, _headTransform.forward);
+        Gizmos.DrawRay(_eyePosVec, _eyeTarget.position - _eyePosVec);
+
+        float zDist = Mathf.Abs(_eyePosVec.z - _eyeTarget.position.z);
+       // Gizmos.DrawLine(_eyeTarget.position, _eyePosVec - new Vector3(0, 0, zDist));
+
+        _eyeDiff = new Vector2(_eyeTarget.position.x - _eyePosVec.x, _eyeTarget.position.y-_eyePosVec.y);
+       // Debug.Log(Vector3.Cross(_headTransform.forward, _eyeTarget.position - _eyePosVec));
+
+        _leftEyePosX = 0.25f + _eyeDiff.x;
+        _leftEyePosX = Mathf.Clamp(_leftEyePosX, 0, 0.5f);
+        _leftEyePosY = 0.475f + _eyeDiff.y;
+        _leftEyePosY = Mathf.Clamp(_leftEyePosY, 0.3f, 0.65f);
+
+        _eyeMaterial.SetFloat("_LPosX", _leftEyePosX);
+        _eyeMaterial.SetFloat("_LPosY", _leftEyePosY);
+        // _eyeMaterial.SetFloat("_LSizeX", _leftEyeSizeX);
+        // _eyeMaterial.SetFloat("_LSizeY", _leftEyeSizeY);
+        
+       // Vector3 _reyeOffset = new Vector3(_eyeOffset.x * -1, _eyeOffset.y, _eyeOffset.z);
+       // Vector3 _rightEyePosVec = _headTransform.position + _reyeOffset;
+        //_rightEyePosVec = _headTransform.TransformPoint(_rightEyePosVec);
+
+       // Gizmos.DrawRay(_rightEyePosVec, _headTransform.forward);
+       // Gizmos.DrawRay(_rightEyePosVec, _eyeTarget.position - _rightEyePosVec);
+        //Gizmos.DrawLine(_eyeTarget.position, _rightEyePosVec - new Vector3(0, 0, zDist));
+        //Vector2 _rEyeDiff = new Vector2(_eyeTarget.position.x - _rightEyePosVec.x, _eyeTarget.position.y - _rightEyePosVec.y);
+
+        //_rightEyePosX = .5f + _eyeDiff.x;
+        //_rightEyePosX = Mathf.Clamp(_rightEyePosX, 0.5f, 1f);
+        //_rightEyePosY = .475f + _eyeDiff.y;
+        //_rightEyePosY = Mathf.Clamp(_rightEyePosY, 0.3f, 0.65f);
+
+
+        
+
+        //_eyeMaterial.SetFloat("_RPosX", _rightEyePosX);
+        //_eyeMaterial.SetFloat("_RPosY", _rightEyePosY);
+       // _eyeMaterial.SetFloat("_RSizeX", _rightEyeSizeX);
+       // _eyeMaterial.SetFloat("_RSizeY", _rightEyeSizeY);
     }
 }
