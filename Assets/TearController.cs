94ca1153afc9efe89eb14ac1016163ba0dcd678b
Easton <eastonself@gmail.com>
2024-01-30 16:51:08 -0500

    no err

--- Assets/TearController.cs
+++ Assets/TearController.cs
@@ -7,7 +7,7 @@ public class TearController : MonoBehaviour
     public Transform tearObject;
 
     void Update(){
-        Vector3 eyeHeight = transform.TransformPoint(new Vector3(0, -.2f, 0f));
+        Vector3 eyeHeight = transform.TransformPoint(new Vector3(0, -.4f, 0f));
         Vector3 floorHeight = new Vector3(eyeHeight.x, -2f, -.4f);
         tearObject.position = (eyeHeight + floorHeight)/2f + Vector3.up*.3f;
         tearObject.localScale = new Vector3(.1f, eyeHeight.y - floorHeight.y, 1f);
